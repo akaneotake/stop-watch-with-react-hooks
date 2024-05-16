9631ae10ffcac5f017daa07e8086db8885bc3334
Akane <akane1033@gmail.com>
2024-05-16 12:46:41 +0200

    Finished, but not currect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 const StopWatch = () => {
-  let [timePassedInMilliSeconds, setTimePassed] = useState(0);
-  let timer = null;
+  const [timePassedInMilliSeconds, setTimePassed] = useState(0);
 
-  const start = () => {};
+  const timer = useRef(null);
+
+  const start = () => {
+    if(!timer.current) {
+      let startTime = Date.now();
+      timer.current = setInterval(() => {
+        console.log('timePassedInMilliSeconds: ', timePassedInMilliSeconds);
+        
+        const stopTime= Date.now();
+
+        setTimePassed(timePassedInMilliSeconds => stopTime - startTime + timePassedInMilliSeconds);
+
+        startTime= stopTime;
+      }, 1000);
+    }
+  };
   
-  const stop = () => {};
+  const stop = () => {
+    console.log('stop', timer.current);
+    window.clearInterval(timer.current);
+    timer.current= null;
+  };
   
-  const reset = () => {};
+  const reset = () => {
+    stop();
+    setTimePassed(0);
+  };
 
   return (
     <div>
       <h2 className="border px-3 py-4 rounded my-3 mx-auto text-center" style={{maxWidth: "300px"}}>
         {Math.floor(timePassedInMilliSeconds / 1000)} s
       </h2>
+
       <div className="d-flex justify-content-center">
         <button className="btn btn-outline-primary mr-2" onClick={start}>
           start
         </button>
+
         <button className="btn btn-outline-danger mr-2" onClick={stop}>
           stop
         </button>
+
         <button className="btn btn-outline-warning" onClick={reset}>
           reset
         </button>
